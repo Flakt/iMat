d6abad48a27f1fb2d50bfa3a306793f06129ca27
tomraj <tommy.rajert98@gmail.com>
2018-04-30 14:55:52 +0200

    smthn

--- src/matbutik/IMatController.java
+++ src/matbutik/IMatController.java
@@ -6,19 +6,22 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class IMatController implements Initializable {
 
     @FXML
-    private ListView categories;
+    private ListView<String> categories;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        categories = new ListView<>();
         categories.setItems(FXCollections.observableArrayList(
                 "Erbjudanden", "Mejeri", "Chark & Pålägg", "Fisk", "Frukt & Grönt", "Bröd", "Kryddhyllan",
                 "Dryck", "Frys", "Konfektyr & Kaffebröd"));
 
+        
     }
 }
