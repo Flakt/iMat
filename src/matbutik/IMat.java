a2ab89a771a7b32d2b7d76c41e0e4512ac7b27a3
tomraj <tommy.rajert98@gmail.com>
2018-04-30 14:02:37 +0200

    Lite smått

--- src/matbutik/IMat.java
+++ src/matbutik/IMat.java
@@ -1,9 +1,12 @@
 package matbutik;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.util.ResourceBundle;
@@ -11,6 +14,9 @@ import java.util.ResourceBundle;
 import static javafx.application.Application.launch;
 
 public class IMat extends Application {
+
+    @FXML
+        private ListView categories;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -32,5 +38,10 @@ public class IMat extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
+        /*categories.setItems(FXCollections.observableArrayList(
+                "Erbjudanden", "Mejeri", "Chark & Pålägg", "Fisk", "Frukt & Grönt", "Bröd", "Kryddhyllan",
+                "Dryck", "Frys", "Konfektyr & Kaffebröd"));
+        */
     }
 }
