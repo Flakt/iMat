f51f2ed0645599403c66f1e2738d301015336c5f
tomraj <tommy.rajert98@gmail.com>
2018-04-30 17:44:47 +0200

    Färg och shit till menyn

--- src/matbutik/IMatController.java
+++ src/matbutik/IMatController.java
@@ -1,9 +1,14 @@
 package matbutik;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableListBase;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -12,16 +17,15 @@ import java.util.ResourceBundle;
 public class IMatController implements Initializable {
 
     @FXML
-    private ListView<String> categories;
+        private ListView<String> categories;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        categories = new ListView<>();
-        categories.setItems(FXCollections.observableArrayList(
+        //categories = new ListView<>();
+        categories.getItems().addAll(
                 "Erbjudanden", "Mejeri", "Chark & Pålägg", "Fisk", "Frukt & Grönt", "Bröd", "Kryddhyllan",
-                "Dryck", "Frys", "Konfektyr & Kaffebröd"));
-
-        
+                "Dryck", "Frys", "Konfektyr & Kaffebröd");
+        categories.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
     }
 }
