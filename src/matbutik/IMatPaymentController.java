ff9238835b42b147928109832917542d03161a6b
Stefan Chan <stefan2846@hotmail.com>
2018-05-08 12:33:40 +0200

    Implementering betalning
    Inte testad, vet inte om backend kommer att krångla till det

--- src/matbutik/IMatPaymentController.java
+++ src/matbutik/IMatPaymentController.java
@@ -1,9 +1,15 @@
 package matbutik;
 
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.CreditCard;
+import se.chalmers.cse.dat216.project.Customer;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 import java.net.URL;
@@ -12,6 +18,8 @@ import java.util.ResourceBundle;
 public class IMatPaymentController implements Initializable {
 
     private IMatDataHandler dataHandler;
+    private Customer customer;
+    private CreditCard creditCard;
 
     @FXML
     private AnchorPane cartDetailsPane;
@@ -32,7 +40,9 @@ public class IMatPaymentController implements Initializable {
     @FXML
     private Button creditPayButton;
     @FXML
-    private CheckBox saveDetailsCheckBox;
+    private CheckBox creditSaveDetailsCheckBox;
+    @FXML
+    private CheckBox invoiceSaveDetailsCheckBox;
     @FXML
     private Label numberOfProductsLabel;
     @FXML
@@ -68,11 +78,95 @@ public class IMatPaymentController implements Initializable {
     @FXML
     private TextField monthTextField;
     @FXML
-    private TextField cvvcvcTextField;
-
+    private TextField verificationCodeTextField;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         dataHandler = IMatDataHandler.getInstance();
+        customer = dataHandler.getCustomer();
+        creditCard = dataHandler.getCreditCard();
+        paymentOptionsSplitPane.toFront();
     }
+
+    private void invoiceInit() {
+        invoiceNumberOfProductsLabel.setText("Antal Varor: " + String.valueOf(dataHandler.getShoppingCart().getItems().size()));
+        invoiceSumLabel.setText("Summa: " + String.valueOf(dataHandler.getShoppingCart().getTotal()));
+        headerLabel.setText("Du har valt att betala med faktura");
+        forenameTextField.focusedProperty().addListener(new TextFieldListener(forenameTextField));
+        surnameTextField.focusedProperty().addListener(new TextFieldListener(surnameTextField));
+        adressTextField.focusedProperty().addListener(new TextFieldListener(adressTextField));
+        zipcodeTextField.focusedProperty().addListener(new TextFieldListener(zipcodeTextField));
+        regionTextField.focusedProperty().addListener(new TextFieldListener(regionTextField));
+    }
+
+    private void creditInit() {
+        creditNumberOfProductsLabel.setText("Antal Varor: " + String.valueOf(dataHandler.getShoppingCart().getItems().size()));
+        creditSumLabel.setText("Summa: " + String.valueOf(dataHandler.getShoppingCart().getTotal()));
+        headerLabel.setText("Du har valt att betala med kreditkort");
+        nameTextField.focusedProperty().addListener(new TextFieldListener(nameTextField));
+        cardNumberTextField.focusedProperty().addListener(new TextFieldListener(cardNumberTextField));
+        monthTextField.focusedProperty().addListener(new TextFieldListener(monthTextField));
+        yearTextField.focusedProperty().addListener(new TextFieldListener(yearTextField));
+        verificationCodeTextField.focusedProperty().addListener(new TextFieldListener(verificationCodeTextField));
+    }
+
+    @FXML
+    protected void invoiceOnClick(Event event) {
+        // Maybe run init invoice page here (If not initialized in initialize)
+        invoiceAnchorPane.toFront();
+    }
+
+    @FXML
+    protected void creditOnClick(Event event) {
+        // Maybe run init credit page here (If not already initialized)
+        creditcardAnchorPane.toFront();
+    }
+
+    @FXML
+    protected void invoicePayAction() {
+        if (invoiceSaveDetailsCheckBox.isSelected()) {
+            customer.setFirstName(forenameTextField.getText());
+            customer.setLastName(surnameTextField.getText());
+            customer.setAddress(adressTextField.getText());
+            customer.setPostAddress(regionTextField.getText());
+            customer.setPostCode(zipcodeTextField.getText());
+        }
+        // Redirect to "you have payed"
+    }
+
+    @FXML
+    protected void creditPayAction() {
+        if (creditSaveDetailsCheckBox.isSelected()) {
+            creditCard.setHoldersName(nameTextField.getText());
+            creditCard.setCardNumber(cardNumberTextField.getText());
+            creditCard.setValidMonth(Integer.parseInt(monthTextField.getText()));
+            creditCard.setValidYear(Integer.parseInt(yearTextField.getText()));
+            creditCard.setVerificationCode(Integer.parseInt(verificationCodeTextField.getText()));
+        }
+        // Redirect to "you have payed"
+    }
+
+    private class TextFieldListener implements ChangeListener<Boolean> {
+        private TextField textField;
+
+        public TextFieldListener(TextField textField) {
+            this.textField = textField;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if (newValue) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        textField.selectAll();
+                    }
+                });
+            }
+            else {
+                // Maybe put something here later on
+            }
+        }
+    }
+
 }
