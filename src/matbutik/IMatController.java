3308b4b3050d4ecdaf5277f04e308fd1297f3e93
Philip <philip.warfvinge@outlook.com>
2018-05-14 11:35:37 +0200

    Gjorde det här till kontrollern för startsidan. Den laddar nu in produkter.

--- src/matbutik/IMatController.java
+++ src/matbutik/IMatController.java
@@ -12,15 +12,29 @@ import javafx.scene.layout.StackPane;
 import javafx.util.Callback;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.net.URL;
 import java.util.*;
+import java.util.concurrent.Flow;
 
 public class IMatController implements Initializable {
 
+
+    @FXML FlowPane mainPage;
+
+    IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        listItems();
         }
+
+        private void listItems(){
+
+        for (Product product : dataHandler.getProducts()) {
+            mainPage.getChildren().add(new IMatProductItem(product, dataHandler));
+        }
+    }
 
 }
