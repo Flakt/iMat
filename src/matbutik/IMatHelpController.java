0c9bfc7e95502398abaa58e8596f20d554b14ff4
Bladish <Bladish@users.noreply.github.com>
2018-05-18 18:01:23 +0200

    IMatHelp knappar funkar

--- src/matbutik/IMatHelpController.java
+++ src/matbutik/IMatHelpController.java
@@ -4,11 +4,13 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class IMatHelpController implements Initializable {
     IMatNavigationHandler navigationHandler = IMatNavigationHandler.getInstance();
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -22,5 +24,12 @@ public class IMatHelpController implements Initializable {
     public void reselectTabs() {
 
     }
+
+    @FXML public void shoppingCart(Event event){
+        navigationHandler.toDestination("ShoppingCart");
+    }
+    @FXML public void navigateGoBack(Event event){navigationHandler.goBack();}
+
+    @FXML public void navigateToAccount(Event event){navigationHandler.toAccount();}
 }
 
