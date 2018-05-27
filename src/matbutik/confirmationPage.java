d389ab6717897d4a3173bb62cdbc28959e26e557
Philip <philip.warfvinge@outlook.com>
2018-05-27 21:42:35 +0200

    Kvitto!

--- src/matbutik/confirmationPage.java
+++ src/matbutik/confirmationPage.java
@@ -4,7 +4,12 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import se.chalmers.cse.dat216.project.Customer;
+import se.chalmers.cse.dat216.project.Order;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,9 +17,38 @@ public class confirmationPage extends IMatController implements Initializable {
 
 
 
+    @FXML Label name;
+    @FXML Label time;
+    @FXML Label address;
+    @FXML Label date;
+    @FXML Label city;
+
+
+    @FXML
+    FlowPane receiptFlowPane;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Order order = dataHandler.getOrders().get(dataHandler.getOrders().size()-1);
+        Customer customer = dataHandler.getCustomer();
+
+
+        name.setText(customer.getFirstName() + " " + customer.getLastName());
+        address.setText(customer.getAddress());
+        city.setText(customer.getPostAddress());
+
+
+        for (ShoppingItem shoppingItem : order.getItems()) {
+            receiptFlowPane.getChildren().add(new ReceiptListItem(shoppingItem));
+            System.out.println("Element added to receipt FlowPane");
+        }
+
+
+     //   time.setText();
+     //   city.setText();
+
+
     }
 
     @FXML
